70b31ae6d0762b351ae7ae0611d372b42457f7aa
Kevin Simpson <relasine@gmail.com>
2018-11-22 17:05:47 -0700

    change background to background-image

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../css/App.css";
 
+import Preloading from "./Preloading";
 import Forest from "./Forest";
 
 class App extends Component {
@@ -8,6 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Forest />
+        <div className="preloading">
+          <Preloading />
+        </div>
       </div>
     );
   }
---
+++ src/components/Preloading.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import claw1 from "../css/images/effects/claw/claw_1.png";
+import claw2 from "../css/images/effects/claw/claw_2.png";
+import claw3 from "../css/images/effects/claw/claw_3.png";
+import claw4 from "../css/images/effects/claw/claw_4.png";
+import claw5 from "../css/images/effects/claw/claw_5.png";
+import claw6 from "../css/images/effects/claw/claw_6.png";
+
+import slash1 from "../css/images/effects/slash/slash1.png";
+import slash2 from "../css/images/effects/slash/slash2.png";
+import slash3 from "../css/images/effects/slash/slash3.png";
+import slash4 from "../css/images/effects/slash/slash4.png";
+import slash5 from "../css/images/effects/slash/slash5.png";
+
+const Preloading = () => {
+  return (
+    <div>
+      <img src={claw1} alt="claw1" />
+      <img src={claw2} alt="claw2" />
+      <img src={claw3} alt="claw3" />
+      <img src={claw4} alt="claw4" />
+      <img src={claw5} alt="claw5" />
+      <img src={claw6} alt="claw6" />
+      <img src={slash1} alt="slash1" />
+      <img src={slash2} alt="slash2" />
+      <img src={slash3} alt="slash3" />
+      <img src={slash4} alt="slash4" />
+      <img src={slash5} alt="slash5" />
+    </div>
+  );
+};
+
+export default Preloading;
